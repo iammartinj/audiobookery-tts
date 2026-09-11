a5360a08ee3b9c567f7cfe2ed0c4021958dfc934
martin <163653078+iammartinj@users.noreply.github.com>
2026-09-11 13:21:43 +0200

    Release 1.8.0

--- audiobookery.py
+++ audiobookery.py
@@ -56,7 +56,7 @@ APP_NAME = "Audiobookery"
 
 # Znaky, které Windows v názvu souboru nedovolí
 ZAKAZANE_ZNAKY = r'[<>:"/\|?*]'
-VERSION = "1.7.0"
+VERSION = "1.8.0"
 
 VYSLOVNOST_PATH = APP_DIR / "vyslovnost.json"
 
